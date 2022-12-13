a0b03541e0a372d95e76b32e6e0741103b6e428e
Tishly <ekaetteessien27@gmail.com>
2022-12-13 10:33:40 +0100

    modified db variables string

--- app/database.py
+++ app/database.py
@@ -7,7 +7,7 @@ from psycopg2.extras import RealDictCursor
 import time
 
 
-SQLALCHEMY_DATABASE_URL = 'postgres://paddy_user:WcBkrp5xE54mUGws2rFKFRLebHd2jRA7@dpg-cebp98da49965ve4odug-a/paddy'
+SQLALCHEMY_DATABASE_URL = 'postgresql://paddy_user:WcBkrp5xE54mUGws2rFKFRLebHd2jRA7@dpg-cebp98da49965ve4odug-a/paddy'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
