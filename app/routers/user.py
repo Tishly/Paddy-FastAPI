14fc22bdf0cda973187e253a8b999e63bf2058fa
Tishly <ekaetteessien27@gmail.com>
2022-12-14 23:48:10 +0100

    Corrected line-up of endpoints in routes/user

--- app/routers/user.py
+++ app/routers/user.py
@@ -20,16 +20,16 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db.refresh(new_user)
     return new_user
 
-@router.get("/{id}", response_model=schemas.UserOutput)
-def get_user(id: int, db: Session = Depends(get_db)):
-    user = db.query(models.User).filter(models.User.id == id).first()
-    if not user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"There is no user with id: {id}")
-    return user
-
 @router.get("/all", response_model=schemas.UserOutput)
 def get_user(db: Session = Depends(get_db)):
     user = db.query(models.User).all()
     # if not user:
     #     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"There is no user with id: {id}")
+    return user
+
+@router.get("/{id}", response_model=schemas.UserOutput)
+def get_user(id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"There is no user with id: {id}")
     return user
