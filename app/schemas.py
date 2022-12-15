31a95f3c92c54c4f1e9c0fab27fba7da730d854d
Tishly <ekaetteessien27@gmail.com>
2022-12-15 03:35:13 +0100

    Uncommented greyed-out schemas

--- app/schemas.py
+++ app/schemas.py
@@ -28,13 +28,13 @@ class Post(PostBase):
     class Config:
         orm_mode = True
 
-# class UserCreate(BaseModel):
-#     email:EmailStr
-#     password: str
+class UserCreate(BaseModel):
+    email:EmailStr
+    password: str
 
-# class UserLogin(BaseModel):
-#     email: EmailStr
-#     password: str
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
    
 
 class Token(BaseModel):
